a443d870fbb6d8b5f377ede774fc5916a1319bc2
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-07-25 17:44:06 +0300

    Update filterfalse.hpp

--- filterfalse.hpp
+++ filterfalse.hpp
@@ -5,72 +5,63 @@
 
 namespace itertools
 {
-    template<typename T, typename F>
+    template  <typename T,typename F>
     class filterfalse
     {
-        private:
-            T cont;
+        T cont;
+        F func;
+    public:
+        filterfalse (F f,T c)  : cont(c), func(f) {}
+        class iterator
+        {
+            typename T::iterator first_val;
+            typename T::iterator last_val;
             F func;
+
         public:
-            filterfalse(F f, T c) : cont(c), func(f) {}
-
-            class iterator
+            iterator(typename T::iterator start, typename T::iterator end, F f) : first_val(start), last_val(end), func(f){}
+            auto operator*()
             {
-                private:
-                    typename T::iterator first_val;
-                    typename T::iterator last_val;
-                    F func;
-                public:
-                    iterator(typename T::iterator first, typename T::iterator last, F f) : first_val(first), last_val(last),
-                                                                                   func(f) {}
+                while(func(*first_val))
+                    ++(*this);
+                return *first_val;
+            }
+            const iterator &operator++()
+            {
+                ++first_val;
+                while(first_val!=last_val&&func(*first_val))
+                    ++first_val;
+                return *this;
+            }
+            iterator operator++(int)
+            {
+                iterator copy=*this;
+                ++first_val;
+                while(first_val!=last_val&&func(*first_val))
+                    ++first_val;
+                return copy;
+            }
+            iterator& operator=(const iterator& other)
+            {
+                if (*this != other)
+                {
+                    this->last_val=other.last_val;
+                    this->first_val=other.first_val;
+                    this->func=other.func;
+                }
+                return *this;
+            }
+            bool operator==(const iterator &other) const
+            {
+                return this->first_val == other.first_val;
+            }
+            bool operator!=(const iterator &other) const
+            {
+                return this->first_val != other.first_val;
+            }
+        };//class iterator
+        iterator begin() {return iterator  (cont.begin(),cont.end(),func);}
+        iterator end() { return iterator (cont.end(),cont.end(),func); }
 
-                    bool operator==(const iterator &other) { return first_val == other.first_val; }
-
-                    bool operator!=(const iterator &other) const { return first_val != other.first_val; }
-                    iterator& operator=(const iterator& other)
-                    {
-                        if(*this != other)
-                        {
-                            this->last_val = other.last_val;
-                            this->first_val = other.first_val;
-                            this->func = other.func;
-                        }
-                        return *this;
-                    }
-                    auto operator*()
-                    {
-                        while (func(*first_val))
-                        {
-                            ++(*this);
-                        }
-                        return first_val;
-                    }
-
-                    //++first_val
-                   const iterator& operator++()
-                    {
-                        ++first_val;
-                        while (first_val != last_val && func(*first_val))
-                        {
-                            ++first_val;
-                        }
-                        return *this;
-                    }
-
-                    //first_val++
-                    iterator operator++(int)
-                    {
-                        iterator copy = *this;
-                        ++first_val;
-                        while (first_val != last_val && func(*first_val))
-                        {
-                            ++first_val;
-                        }
-                        return copy;
-                    }
-                };
-
-                iterator begin() {return iterator(cont.begin(),cont.end(),func);}
-                iterator end() {return iterator(cont.end(),cont.end(),func);}
-    };
-}
+    }; //class filterfalse
+}//namespace itertools 
