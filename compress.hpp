af0cc361630f50dc720ca0bb366634fe5962301f
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-07-25 17:48:40 +0300

    Update compress.hpp

--- compress.hpp
+++ compress.hpp
@@ -1,66 +1,75 @@
 //
 // Created by rotem levy on 11/06/2020.
 //
-
-#include <iostream>
-using namespace std;
+#pragma once
 
 namespace itertools
 {
-    template<typename T,typename F>
+    template  <typename T,typename F>
     class compress
     {
-            private:
-                T container1;
-                F container2;
-            public:
-                compress (T c1,F c2)  : container1(c1), container2(c2) {}
-                class iterator
-                {
-                    typename T::iterator first_val1;
-                    typename T::iterator last_val1;
-                    typename F::iterator first_val2;
-                    typename F::iterator last_val2;
+        T container1;
+        F container2;
+    public:
+        compress (T con1,F con2)  : container1(con1), container2(con2) {}
+        class iterator
+        {
+            typename T::iterator first_val;
+            typename T::iterator last_val;
+            typename F::iterator first_flag;
+            typename F::iterator last_flag;
 
-                    public:
-                        iterator(typename T::iterator first1, typename T::iterator last1,typename F::iterator first2, typename F::iterator last2) : first_val1(first1), last_val1(last1),first_val2(first2), last_val2(last2) {}
-                         auto operator*()
-                        {
-                            while(first_val1!=last_val1&&!*first_val2)
-                            {
-                                ++(*this);
-                            }
-                            return *first_val1;
-                        }
-                        //++first_val
-                        iterator &operator++()
-                        {
-                            ++first_val1;
-                            ++first_val2;
-                            while(first_val1!=last_val1&&!*first_val2)
-                            {
-                                ++first_val1;
-                                ++first_val2;
-                            }
-                            return *this;
-                        }
-                        //first_val++
-                        iterator operator++(int)
-                        {
-                             iterator copy=*this;
-                             ++first_val1;
-                             ++first_val2;
-                             while(first_val1!=last_val1 && !*first_val2)
-                            {
-                                ++first_val1;
-                                ++first_val2;
-                            }
-                             return copy;
-                        }
-                         bool operator==(const iterator &other) const { return first_val1 == other.first_val1;}
-                         bool operator!=(const iterator &other) const { return first_val1 != other.first_val1;}
-                };
-                iterator begin() {return iterator(container1.begin(),container1.end(),container2.begin(),container2.end());}
-                iterator end() { return iterator(container1.end(),container1.end(),container2.end(),container2.end()); }
-    };
-}
+        public:
+            iterator(typename T::iterator start1, typename T::iterator end1,typename F::iterator start2, typename F::iterator end2) : first_val(start1), last_val(end1),first_flag(start2), last_flag(end2) {}
+            auto operator*()
+            {
+                while(first_val!=last_val&&!*first_flag)
+                    ++(*this);
+                return *first_val;
+            }
+            iterator &operator++()
+            {
+                ++bIter;
+                ++bIterBool;
+                while(first_val!=last_val&&!*first_flag)
+                {
+                    ++first_val;
+                    ++first_flag;
+                }
+                return *this;
+            }
+            iterator operator++(int)
+            {
+                iterator copy=*this;
+                ++first_val;
+                ++first_flag;
+                while(first_val!=last_val&&!*first_flag)
+                {
+                    ++first_val;
+                    ++first_flag;
+                }
+                return copy;
+            }
+            iterator& operator=(const iterator& other)
+            {
+                if (*this != other)
+                {
+                    this->last_val=other.last_val;
+                    this->first_val=other.first_val;
+                    this->func=other.func;
+                }
+                return *this;
+            }
+            bool operator==(const iterator &other) const
+            {
+                return this->first_val == other.first_val;
+            }
+            bool operator!=(const iterator &other) const
+            {
+                return this->first_val != other.first_val;
+            }
+        };//class iterator
+        iterator begin() {return iterator  (container1.begin(),container1.end(),container2.begin(),container2.end());}
+        iterator end() { return iterator (container1.end(),container1.end(),container2.end(),container2.end()); }
+    }; //class compress
+}//namespace itertools 
