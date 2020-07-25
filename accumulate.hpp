267eec028de24843cb81ae64a62ac50ea9d37f1b
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-07-25 17:52:08 +0300

    Update accumulate.hpp

--- accumulate.hpp
+++ accumulate.hpp
@@ -8,69 +8,68 @@ namespace itertools
     typedef struct
     {
         template <typename P>
-        P operator()(P x, P y) const
+        P operator ()(P x, P y) const
         {
             return x+y;
         }
     }plus;
-
-    template<typename T, typename F=plus>
+    
+    template  <typename T,typename F=plus>
     class accumulate
     {
-        private:
-             T cont;
-             F func;
-            typedef typename T::value_type value_type;
+        T cont;
+        F func;
+        typedef typename T::value_type value_type;
+    public:
+        accumulate (T c,F f=plus()) : cont(c), func(f) {}
+
+        class iterator
+        {
+            typename T::iterator first_val;
+            typename T::iterator last_val;
+            typename T:: value_type result;
+            F func;
+
+
         public:
-            accumulate(T c, F f=plus()): cont(c), func(f) {};
-
-            class iterator
+            iterator(typename T::iterator start, typename T::iterator end, F f) : first_val(start), last_val(end), func(f), result(*first_val){}
+            auto operator*() const {return result;}
+            iterator &operator++()
             {
-                private:
-                   typename T::iterator first_val;
-                   typename T::iterator last_val;
-                   typename T::value_type result;
-                    F func;
-
-                public:
-                    iterator(typename T::iterator first, typename T::iterator last, F f) : first_val(first), last_val(last), func(f), result(*first_val){}
-                    auto operator*() const {return result;}
-                    iterator& operator++()
-                    {
-                        ++first_val;
-                        if(first_val!=last_val)
-                        {
-                            result = func(result,*first_val);
-                        }
-                        return *this;
-                    }
-                    iterator operator++(int)
-                    {
-                        iterator copy=*this;
-                        ++first_val;
-                        if(first_val!=last_val)
-                        {
-                            result = func(result,*first_val);
-                        }
-                        return copy;
-                    }
-                    
-                    iterator& operator=(const iterator& other)
-                    {
-                        if(*this != other)
-                        {
-                            this->first_val = other.first_val;
-                            this->last_val = other.last_val;
-                            this->func = other.func;
-                            this->result = other.result;
-                        }
-                        return *this;
-                    }
-                    bool operator==(const iterator &other) const { return first_val == other.first_val; }
-                    bool operator!=(const iterator &other) const { return first_val != other.first_val; }
-            };//class iterator
-
-            iterator begin() {return iterator(cont.begin(),cont.end(),func);}
-            iterator end() { return iterator(cont.end(),cont.end(),func);}
-    };
-};
+                ++first_val;
+                if(first_val!=last_val)
+                    result=func(result,*first_val);
+                return *this;
+            }
+            iterator operator++(int)
+            {
+                iterator copy=*this;
+                ++first_val;
+                if(first_val!=last_val)
+                    result=func(result,*first_val);
+                return copy;
+            }
+            iterator& operator=(const iterator& other)
+            {
+                if (*this != other)
+                {
+                    this->first_val = other.first_val;
+                    this->result = other.result;
+                    this->last_val = other.last_val;
+                    this->func = other.func;
+                }
+                return *this;
+            }
+            bool operator==(const iterator &other) const
+            {
+                return this->first_val == other.first_val;
+            }
+            bool operator!=(const iterator &other) const
+            {
+                return this->first_val != other.first_val;
+            }
+        };//class iterator
+        iterator begin() {return iterator  (cont.begin(),cont.end(),func);}
+        iterator end() { return iterator (cont.end(),cont.end(),func);}
+    }; //class accumulate
+}//namespace itertools 
